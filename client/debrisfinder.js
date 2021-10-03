7fc7c706722fb2e8b36b8bda935c0618533faec2
sponsoredbycody <codyuehara@gmail.com>
2021-10-02 19:52:22 -0700

    added stars

--- client/debrisfinder.js
+++ client/debrisfinder.js
@@ -1,54 +1,15 @@
+
+
 var wwd = new WorldWind.WorldWindow("wwd");
 wwd.addLayer(new WorldWind.BMNGOneImageLayer());
-wwd.addLayer(new WorldWind.BMNGLandsatLayer());
+wwd.addLayer(new WorldWind.AtmosphereLayer());
+//wwd.addLayer(new WorldWind.BMNGLandsatLayer());
 //wwd.addLayer(new WorldWind.CompassLayer());
-wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
-wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
+//wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
+//wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
+wwd.addLayer(new WorldWind.StarFieldLayer());
 
-var placemarkLayer = new WorldWind.RenderableLayer("Placemark");
-wwd.addLayer(placemarkLayer);
 
-var placemarkAttributes = new WorldWind.PlacemarkAttributes(null);
-placemarkAttributes.imageOffset - new WorldWind.Offset(
-    WorldWind.OFFSET_FRACTION, 0.3,
-    WorldWind.OFFSET_FRACTION, 0.0);
-
-placemarkAttributes.labelAttributes.color = WorldWind.Color.YELLOW;
-placemarkAttributes.labelAttributes.offset = new WorldWind.Offset(
-    WorldWind.OFFSET_FRACTION, 0.5,
-    WorldWind.OFFSET_FRACTION, 1.0
-);
-
-placemarkAttributes.imageSource = WorldWind.configuration.baseUrl + "images/white-dot.png";
-
-var position = new WorldWind.Position(55.0, -106.0, 500.0);
-var placemark = new WorldWind.Placemark(position, true, placemarkAttributes);
-
-placemark.label = "Placemark\n" +
-    "lat " + placemark.position.latitude.toPrecision(4).toString() +
-    "\n" +
-    "lon " + placemark.position.longitude.toPrecision(5).toString();
-placemark.alwaysOnTop = true;
-
-placemarkLayer.addRenderable(placemark);
-
-var polygonLayer = new WorldWind.RenderableLayer();
-wwd.addLayer(polygonLayer);
-
-var polygonAttributes = new WorldWind.ShapeAttributes(null);
-polygonAttributes.interiorColor = new WorldWind.Color(0,0,0, 0.75);
-polygonAttributes.outlineColor = WorldWind.Color.BLACK;
-polygonAttributes.drawOutline = true;
-polygonAttributes.applLighting = true;
-
-var boundaries = [];
-boundaries.push(new WorldWind.Position(20.0, -75.0, 700000.0));
-boundaries.push(new WorldWind.Position(25.0, -85.0, 700000.0));
-boundaries.push(new WorldWind.Position(20.0, -95.0, 700000.0));
-
-var polygon = new WorldWind.Polygon(boundaries, polygonAttributes);
-polygon.extrude = true;
-polygonLayer.addRenderable(polygon);
 
 
 
